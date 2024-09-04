8d18457fd3fca597c16a6c02a68518acf42f79ad
andrewquang512 <andrew.viquang@gmail.com>
2024-09-04 22:58:46 +0700

    learn(component)

--- NOTE.md
+++ NOTE.md
@@ -1,1 +1,6 @@
 bootstrapApplication bootstraps ( loads/ starts) an instance of an Angular application and renders a standalone component as the application's root component unit
+
+To generate component: we can use
+```
+ng generate component <component name>
+```
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+  // learn: public or default will make this property available in html, private will not
+  seletecUser = DUMMY_USERS[randomIndex]
+}
